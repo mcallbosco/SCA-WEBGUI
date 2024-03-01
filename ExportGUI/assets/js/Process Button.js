ff0d42b089aa3c55a4831e61625ee654d7848abe
Miles Calloway <mcallbosco@live.com>
2024-03-01 17:06:06 -0500

    test

--- ExportGUI/assets/js/Process Button.js
+++ ExportGUI/assets/js/Process Button.js
@@ -1,5 +1,5 @@
 document.getElementById('WaitText').hidden = true;
-var url = "ws:localhost:8008";
+var url = "ws:3.138.172.59:8008";
 
 
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
