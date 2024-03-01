c7e167cc9a1aa23a0f2ce15c01d114878fccd987
Miles Calloway <mcallbosco@live.com>
2024-03-01 18:44:06 -0500

    Final Try for Today

--- socketServer.py
+++ socketServer.py
@@ -180,7 +180,7 @@ async def hello(websocket, path):
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 
 
-start_server = websockets.serve(hello, "172.31.0.162", 8008, ssl = ssl_context)
+start_server = websockets.serve(hello, "172.31.0.162", 443, ssl = ssl_context)
 
 
 
