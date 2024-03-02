0f3c4d6d8933ac461e5448ffa9d7274d908a0f05
Miles Calloway <mcallbosco@live.com>
2024-03-01 22:45:54 -0500

    Web Cleanup

--- ExportGUI/assets/js/ProcessButton.js
+++ ExportGUI/assets/js/ProcessButton.js
@@ -1,6 +1,6 @@
 document.getElementById('WaitText').hidden = true;
 
-var url = "ws:34.125.226.98:8080";
+var url = "ws:3.138.172.59:8080";
 
 
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
